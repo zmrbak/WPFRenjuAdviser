c3351f0cf4d67a4efd7c1c769743f2e73a5b09f9
zmrbak <20995658@qq.com>
2020-05-05 17:14:31 +0800

    add code

---
+++ 配套代码/RenjuAdvisor04/RenjuAdvisor/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RenjuAdvisor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IKeyboardMouseEvents m_GlobalHook;
+        List<Process> processWithWindow = new List<Process>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Subscribe();
+            GetSysProcessWithWindow();
+        }
+       
+        public void Subscribe()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.MouseMoveExt += M_GlobalHook_MouseMoveExt;
+        }
+        #region Win32 API
+        [DllImport("user32.dll",EntryPoint = "WindowFromPoint")]
+        public static extern IntPtr WindowFromPoint(int xPoint, int yPoint);
+        #endregion
+
+        private void M_GlobalHook_MouseMoveExt(object sender, MouseEventExtArgs e)
+        {
+            //计算鼠标在哪个窗口上
+            IntPtr handle =WindowFromPoint(e.X, e.Y);
+            foreach (var p in processWithWindow)
+            {
+                if (p.MainWindowHandle == handle)
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.Append($"名称：{p.MainModule.FileVersionInfo.ProductName}{Environment.NewLine}");
+                    stringBuilder.Append($"版本：{p.MainModule.FileVersionInfo.FileVersion}{Environment.NewLine}");
+                    stringBuilder.Append($"描述：{p.MainModule.FileVersionInfo.FileDescription}{Environment.NewLine}");
+                    this.info.Text = stringBuilder.ToString();
+                    break;
+                }
+            }
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                IntPtr handle = WindowFromPoint(e.X, e.Y);
+                foreach (var p in processWithWindow)
+                {
+                    if (p.MainWindowHandle == handle)
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.Append($"名称：{p.MainModule.FileVersionInfo.ProductName}{Environment.NewLine}");
+                        stringBuilder.Append($"版本：{p.MainModule.FileVersionInfo.FileVersion}{Environment.NewLine}");
+                        stringBuilder.Append($"描述：{p.MainModule.FileVersionInfo.FileDescription}{Environment.NewLine}");
+                        this.info.Text = stringBuilder.ToString();
+                        break;
+                    }
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+                m_GlobalHook.MouseDownExt -= M_GlobalHook_MouseMoveExt;
+                m_GlobalHook.Dispose();
+            }
+        }
+
+        private void GetSysProcessWithWindow()
+        {
+            Process[] processes = Process.GetProcesses();
+            processWithWindow.Clear();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowHandle == IntPtr.Zero) continue;
+                if (item.MainWindowTitle.Length == 0) continue;
+                processWithWindow.Add(item);
+            }
+        }
+    }
+}
---
+++ 配套代码/RenjuAdvisor04/Test/Program.cs
@@ -0,0 +1,97 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void Main1(string[] args)
+        {
+            //获得系统当前所有进程
+            Process[] processes = Process.GetProcesses();
+            List<Process> processWithWindow = new List<Process>();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowHandle == IntPtr.Zero) continue;
+                if (item.MainWindowTitle.Length==0) continue;
+
+                processWithWindow.Add(item);
+
+
+                //if (item.MainWindowTitle == "五子棋")
+                //{
+                //    Console.WriteLine(item.ProcessName);
+                //    Console.WriteLine(item.Id);
+                //    //窗口名
+                //    Console.WriteLine(item.MainWindowTitle);
+                //    Console.WriteLine(item.MainModule.FileName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileVersion);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileDescription);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.Comments);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.CompanyName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileName);
+                //    //产品名
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.ProductName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.ProductVersion);
+                //    Console.WriteLine(item.StartTime);
+                //    Console.WriteLine(item.MainWindowHandle);
+                //    break;
+                //}
+            }
+
+            foreach (var item in processWithWindow)
+            {
+                Console.WriteLine(item.MainWindowTitle);
+            }
+
+            Console.ReadLine();
+        }
+
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Subscribe();
+            Console.ReadLine();
+            program.Unsubscribe();
+        }
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            //m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1} Position:\t{2},{3}", e.Button, e.Timestamp,e.X,e.Y);
+
+            // uncommenting the following line will suppress the middle mouse button click
+            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            //m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+    }
+}
