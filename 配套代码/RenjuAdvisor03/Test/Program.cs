7c54e737d90a825f9537f4da818ea3fe521f8c2a
zmrbak <20995658@qq.com>
2020-05-05 16:18:49 +0800

    add code

---
+++ 配套代码/RenjuAdvisor03/Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获得系统当前所有进程
+            Process[] processes = Process.GetProcesses();
+            List<Process> processWithWindow = new List<Process>();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowHandle == IntPtr.Zero) continue;
+                if (item.MainWindowTitle.Length==0) continue;
+
+                processWithWindow.Add(item);
+
+
+                //if (item.MainWindowTitle == "五子棋")
+                //{
+                //    Console.WriteLine(item.ProcessName);
+                //    Console.WriteLine(item.Id);
+                //    //窗口名
+                //    Console.WriteLine(item.MainWindowTitle);
+                //    Console.WriteLine(item.MainModule.FileName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileVersion);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileDescription);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.Comments);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.CompanyName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.FileName);
+                //    //产品名
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.ProductName);
+                //    Console.WriteLine(item.MainModule.FileVersionInfo.ProductVersion);
+                //    Console.WriteLine(item.StartTime);
+                //    Console.WriteLine(item.MainWindowHandle);
+                //    break;
+                //}
+            }
+
+            foreach (var item in processWithWindow)
+            {
+                Console.WriteLine(item.MainWindowTitle);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
