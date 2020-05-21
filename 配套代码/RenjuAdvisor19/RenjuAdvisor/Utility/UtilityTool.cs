ddab98cf0af465d27fe03eea09223edf05b49060
zmrbak <20995658@qq.com>
2020-05-21 15:48:19 +0800

    add code

---
+++ 配套代码/RenjuAdvisor19/RenjuAdvisor/Utility/UtilityTool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenjuAdvisor.Utility
+{
+    public class UtilityTool
+    {
+        /// <summary>
+        /// 获取系统中带有窗口的进程
+        /// </summary>
+        public static List<Process> GetSysProcessWithWindow()
+        {
+            List<Process> processWithWindow = new List<Process>();
+            Process[] processes = Process.GetProcesses();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowHandle == IntPtr.Zero) continue;
+                if (item.MainWindowTitle.Length == 0) continue;
+                processWithWindow.Add(item);
+            }
+            return processWithWindow;
+        }
+
+        /// <summary>
+        /// 获得窗口标题为“五子棋”的进程
+        /// </summary>
+        public static Process GetWzqGameProcess()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowTitle == "五子棋")
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
