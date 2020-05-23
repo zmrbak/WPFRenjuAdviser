118d79181dee2cacfde61ae2a4eded798a348cc9
zmrbak <20995658@qq.com>
2020-05-23 12:49:56 +0800

    add code

---
+++ 配套代码/RenjuAdvisor23/RenjuAdvisor/View/CutBoardWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RenjuAdvisor.View
+{
+    /// <summary>
+    /// CutBoardWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CutBoardWindow : Window
+    {
+        public CutBoardWindow()
+        {
+            InitializeComponent();
+        }
+        //MouseLeftButtonDown="Canvas_MouseLeftButtonDown" MouseMove="Canvas_MouseMove"
+
+        //private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+
+        //}
+
+        //private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        //{
+
+        //}
+    }
+}
